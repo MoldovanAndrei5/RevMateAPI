e569c6ee45a6b9a17addf99998c521c9bb3b1828
moldo <moldovanandrei752@gmail.com>
2026-03-27 17:50:22 +0200

    feat; Fixed CI

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def handler(event, context):
         "body": response.text,
         "isBase64Encoded": False,
     }
-# test
+#teest
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
